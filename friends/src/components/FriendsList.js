e60198c2bc90a8f3b20937261bd87493b91c01c2
April Darger <april.m.darger@gmail.com>
2020-11-10 19:28:38 -0700

    Building out components.

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class FriendsList extends React.Component {
+  state = {
+    friendsList: []
+  }
+
+  render(){
+    return(
+      <div>
+        Hello from the other side...
+      </div>
+    )
+  }
+}
+
+export default FriendsList

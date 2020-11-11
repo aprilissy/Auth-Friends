6d116703936225d75104409095237e22a5e957fd
April Darger <april.m.darger@gmail.com>
2020-11-10 21:01:15 -0700

    added get request within componentDidMount.

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,14 +1,33 @@
 import React from 'react'
 
+import { axiosWithAuth } from './../utils/axiosWithAuth';
+
 class FriendsList extends React.Component {
   state = {
     friendsList: []
   }
 
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = () => {
+    axiosWithAuth()
+    .get("/friends")
+    .then(res=>{ 
+      console.log('res',res);
+      this.setState({
+        friendsList: res.friends
+      })
+    }).catch(err=>{
+      console.log(err);
+    })
+  };
+
   render(){
-    return(
+    return( 
       <div>
-        Hello from the other side...
+        <h1>Dementia Sucks. Remember My Friends</h1>
       </div>
     )
   }

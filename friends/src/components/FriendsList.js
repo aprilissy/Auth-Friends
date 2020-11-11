931d911c5bc2d540a72fae43791a8c15cb1d5f93
April Darger <april.m.darger@gmail.com>
2020-11-10 22:46:51 -0700

    Converted to using hooks. Moved post request for addFriend to component.

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,43 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 
 import { axiosWithAuth } from './../utils/axiosWithAuth';
+import AddFriend from './AddFriend'
 
-class FriendsList extends React.Component {
-  state = {
-    friendsList: []
-  }
+const FriendsList = () => {
+  const [friendsList, setFriendsList] = useState([])
 
-  componentDidMount() {
-    this.getData();
-  }
-
-  getData = () => {
+  const getData = () => {
     axiosWithAuth()
     .get("/friends")
     .then(res=>{ 
       console.log('res',res);
-      this.setState({
-        friendsList: res.data
-      })
+      setFriendsList(res.data)
     }).catch(err=>{
       console.log(err);
     })
   };
 
-  render(){
-    return( 
-      <div>
-        <h1>Dementia Sucks. Remember My Friends</h1>
-        {this.state.friendsList.map(e =>(
-          <div className='friend' key={e.id} >
-            <h2>{e.name}</h2>
-            <p>age: {e.age} years</p>
-            <p>email: {e.email} </p>
-          </div>
-        ))}
-      </div>
-    )
+  const addFriend = body => {
+    //e.preventDefault()
+    axiosWithAuth()
+    .post('/friends', body)
+    .then(res =>{
+      console.log('addFriend res',res);
+      getData()  
+    })
+    .catch(err =>{
+      console.log('err',err);
+    })
   }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return( 
+    <div>
+      <h1>Dementia Sucks. Remember My Friends</h1>
+      <AddFriend addFriend={addFriend}/>
+      {friendsList.map(e =>(
+        <div className='friend' key={e.id} >
+          <h2>{e.name}</h2>
+          <p>age: {e.age} years</p>
+          <p>email: {e.email} </p>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default FriendsList
